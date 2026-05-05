eba9cfed3ffc363989c1d9258731f6430d053b4f
Anthony-Maxwell1 <anthony@thatdev.org>
2026-05-05 10:24:54 +1000

    Fixed build errors

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,27 +1,31 @@
-#include "stb_image.h"
-
 #include "texture.hpp"
+#include "external/stb_image.h"
+
 #include <iostream>
 
 Texture loadTexture(const std::string& path) {
     Texture t;
 
     int channels;
-    unsigned char* img = stbi_load(path.c_str(), &t.w, &t.h, &channels, 1);
+    unsigned char* img = stbi_load(
+        path.c_str(),
+        &t.w,
+        &t.h,
+        &channels,
+        1 // force grayscale
+    );
 
     if (!img) {
         std::cerr << "Failed to load texture: " << path << "\n";
         return t;
     }
 
-    t.data.resize(t.w * t.h);
+    t.data.resize(t.w * t.h);  // ✅ now valid
 
     for (int i = 0; i < t.w * t.h; i++) {
-        t.data[i] = img[i]; // already grayscale (forced via 1 channel)
+        t.data[i] = img[i];
     }
 
     stbi_image_free(img);
-
-    t.alive = true;
     return t;
 }
